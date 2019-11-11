47aaf2b731eb302ac82211b255be97ae6ef284f3
hmuzzal <hmuzzal.diu@gmail.com>
2019-11-12 04:08:25 +0600

    _LayoutDone

---
+++ POS/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace POS.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<POS.Identity.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(POS.Identity.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
